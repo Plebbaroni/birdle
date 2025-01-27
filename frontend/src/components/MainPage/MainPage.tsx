289b04116a95ca750cda00f7d4a76c2df0cda991
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-01-27 22:43:44 +0800

    🪿display bird

--- frontend/src/components/MainPage/MainPage.tsx
+++ frontend/src/components/MainPage/MainPage.tsx
@@ -1,11 +1,27 @@
 import classes from "./MainPage.module.css"
 
 function MainPage() {
-  return (
-    <div className={classes.wrapper}>
-        
-    </div>
-  )
+    const bird = {
+        imageurl: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/305292761/2400",
+        credit: "Luke Seitz"
+    }
+
+    return (
+        <div className={classes.wrapper}>
+            <div className={classes.gameDiv}>
+                <div className={classes.bird}>
+                    <p className={classes.todaysBird}>Today's Bird</p>
+                    <div className={classes.imgAndCredit}>
+                        <img src={bird.imageurl} alt="" className={classes.birdImg}/>
+                        <p className={classes.credit}>© {bird.credit}</p>
+                    </div>
+                </div>
+               <div className={classes.cardsDiv}>
+
+               </div>
+            </div>
+        </div>
+    )
 }
 
 export default MainPage
