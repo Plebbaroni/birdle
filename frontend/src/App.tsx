bb09266c1b96cd2e48495256952391d495bbac11
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-01-27 21:37:29 +0800

    🦅folder setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import MainPage from './components/MainPage/MainPage'
+
 function App() {
 
   return (
     <>
     <div className='page'>
       <Navbar/>
+      <MainPage/>
     </div>
     </>
   )
---
+++ frontend/src/components/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import classes from "./MainPage.module.css"
+
+function MainPage() {
+  return (
+    <div className={classes.wrapper}>
+        
+    </div>
+  )
+}
+
+export default MainPage
