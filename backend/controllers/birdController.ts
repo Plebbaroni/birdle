9e5bfb4ca595e9ceee32babbefe0d2de9b78c934
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-02-04 23:39:33 +0800

    🦜bird controller

--- backend/controllers/birdController.ts
+++ backend/controllers/birdController.ts
@@ -1,19 +1,58 @@
 import birdService from "../services/birdService";
-import { TypedRequest, TypedResponse, BirdBody, BirdIdRequest, GuessRequest, UserRequest, GameState } from "../requestTypes";
+import express, { Request, Response } from "express";
+import { TypedRequest, TypedResponse, BirdBody, BirdIdRequest, GuessRequest, UserRequest, GameState, Bird } from "../requestTypes";
+
 class birdController {
-    async getBirdOfTheDay(req:Request, res:TypedResponse<BirdBody>) {
-
+    async getBirdOfTheDay(req:Request, res:Response) {
+        try {
+            const bird = await birdService.getBirdOfTheDay();
+            return res.status(200).json({bird: bird});
+        } catch (error) {
+            return res.status(500).json({error: error});
+        }
     }
 
-    async getBirdById(req:TypedRequest<BirdIdRequest>, res:TypedResponse<BirdBody>) {
+    async getBirdById(req:TypedRequest<BirdIdRequest>, res:Response) {
+        try {
+            const id = req.body.id;
+            if (!id) {
+                return res.status(400).json({error: "Invalid Id"});
+            }
+            const bird = await birdService.getBirdById(id);
+            return res.status(200).json({bird: bird});
 
+        } catch (error) {
+            return res.status(500).json({error: error});
+        }
     }
 
-    async guessBird(req:TypedRequest<GuessRequest>, res:TypedResponse<GameState>) {
+    async guessBird(req:TypedRequest<GuessRequest>, res:Response) {
+        try {
+            const { birdId, userId } = req.body;
+            if (!birdId || !userId) {
+                return res.status(400).json({error: "Invalid input"});
+            }
 
+            const state = await birdService.guessBird(birdId, userId);
+            return res.status(200).json({state: state});
+        } catch (error) {
+            return res.status(500).json({error: error});
+        }
     }
 
-    async getUserState(req:TypedRequest<UserRequest>, res:TypedResponse<GameState>) {
+    async getUserState(req:TypedRequest<UserRequest>, res:Response) {
+        try {
+            const id = req.body.userId;
+            if (!id) {
+                return res.status(400).json({error: "Invalid input"});
+            }
 
+            const state = await birdService.getUserState(id);
+            return res.status(200).json({state:state});
+        } catch (error) {
+            return res.status(500).json({error: error});
+        }
     }
 }
+
+export default new birdController();
