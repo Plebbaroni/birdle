15d9adf73c3601d0e00c72067624ed9f6c7ba2b6
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-02-10 08:09:13 +1100

    🦤search bar shows results and fills input

--- frontend/src/components/InputBar/InputBar.tsx
+++ frontend/src/components/InputBar/InputBar.tsx
@@ -6,33 +6,39 @@ function InputBar() {
 
   const [input, setInput] = useState("");
   const [results, setResults] = useState([]);
+  const [isClicked, setIsClicked] = useState<boolean>(false);
 
   useEffect(() => {
     if (input.length >= 2) {
       let lowerQuery = input.toLowerCase();
       lowerQuery.concat('  ');
       const res = Object.keys(birds).filter(bird => bird.toLowerCase().includes(lowerQuery));
-      console.log(res);
       setResults(res);
+      setIsClicked(false);
     } else {
       setResults([]);
     }
   }, [input]);
 
+  const fillBar = (result:string) => {
+    setInput(result);
+    setResults([]);
+    setIsClicked(true)
+  }
 
   return (  
     <div className={classes.wrapper}>
         <div className={classes.inputBar}>
-            <input type="text" name="" id="" className={classes.inputText} value={input} onChange={(e) => setInput(e.target.value)} placeholder="Enter Your Guess Here"/>
-            {results.length > 0 && (
+          <input type="text" name="" id="" className={classes.inputText} value={input} onChange={(e) => setInput(e.target.value)} placeholder="Enter Your Guess Here"/>
+          {results.length > 0 && !isClicked && (
             <ul className={classes.resultsList}>
               {results.map((result, index) => (
-                <li key={index} className={classes.resultItem}>
+                <li key={index} className={classes.resultItem} onClick={() => fillBar(result)}>
                   {result}
                 </li>
               ))}
             </ul>
-        )}
+          )}
         </div>
     </div>
   )
