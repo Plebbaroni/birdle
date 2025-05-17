237955b4eb01bec36fad399f96e595ee0d00673c
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-05-18 00:47:16 +1000

    🪿like winter soldier again

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import MainPage from './components/MainPage/MainPage'
-import CharitiesPage from './components/CharitiesPage/CharitiesPage'
 import HowToPlay from './components/HowToPlay/HowToPlay'
 
 function App() {
